05b40c9c7dcc8f6c6fc8fe7555aa690b545dc425
Saglai <power1800w@gmail.com>
2023-08-21 22:51:10 +0700

    feat: add components, an interface

---
+++ src/app/posts-table/posts-table.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-posts-table',
+  templateUrl: './posts-table.component.html',
+  styleUrls: ['./posts-table.component.css']
+})
+export class PostsTableComponent implements OnInit{
+  posts: Post[] = [];
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.postService.getPosts()
+      .subscribe(posts => {
+        this.posts = posts;
+      })
+  }
+}
