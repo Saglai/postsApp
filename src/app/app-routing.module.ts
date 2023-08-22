9f1fb96630ea3a73319bde330bca941419e8a2c4
Saglai <power1800w@gmail.com>
2023-08-22 23:10:36 +0700

    feat: add sign-in, sign-up paths to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsTableComponent } from './posts-table/posts-table.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'posts', component: PostsTableComponent},
-  { path: 'detail/:id', component: PostDetailComponent}
+  { path: 'detail/:id', component: PostDetailComponent},
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'sign-up', component: SignUpComponent}
 ];
 
 @NgModule({
