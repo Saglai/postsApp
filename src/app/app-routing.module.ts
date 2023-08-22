8e2599296ff256b7f699542323bf44128a3d50eb
Saglai <power1800w@gmail.com>
2023-08-22 16:54:24 +0700

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsTableComponent } from './posts-table/posts-table.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', component: PostsTableComponent},
+  { path: 'detail/:id', component: PostDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
