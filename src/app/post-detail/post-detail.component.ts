d9e4b173a40a1b358ddeaf655597e35c1583f979
Saglai <power1800w@gmail.com>
2023-08-22 16:55:06 +0700

    feat: add getPost by Id fn, refactor: getPosts fn

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css']
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit {
+  post: Post | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.postService.getPost(id)
+      .subscribe(post => this.post = post);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Post } from './post';
 
@@ -13,6 +14,29 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.postsUrl);
+    return this.http.get<Post[]>(this.postsUrl)
+      .pipe(
+        // tap сообщение пользователю
+        tap(_ => console.log('fetched posts')),
+        catchError(this.handleError<Post[]>('getPosts', []))
+      )
+  }
+
+  getPost(id: number): Observable<Post> {
+    const url =  `${this.postsUrl}/${id}`;
+    return this.http.get<Post>(url).pipe(
+      // tap сообщение пользователю
+      tap(_ => console.log('fetched post id={id}')),
+      catchError(this.handleError<Post>(`getPost id ${id}`))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // Добавить вывод сообщение пользователю
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
